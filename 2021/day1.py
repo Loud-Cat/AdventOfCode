3804fc7aee765ec0204cc998cb9e2cfa37bf0bc6
Edison Ditto <73842360+MedisonMan@users.noreply.github.com>
2021-12-01 00:12:32 -0600

    Create day1.py

---
+++ 2021/day1.py
@@ -0,0 +1,21 @@
+# Read input data
+with open("Inputs/input1.txt") as f:
+  data = [int(i.strip()) for i in f]
+
+# PART ONE
+print("PART ONE")
+total = 0
+for i in range(1, len(data)):
+  if data[i-1] < data[i]:
+    total += 1
+print(total)
+print()
+
+# PART TWO
+print("PART TWO")
+total = 0
+window = data[:3]
+for i in range(1, len(data)):
+  if sum(window) < sum( window := data[i:i+3]):
+    total += 1
+print(total)
