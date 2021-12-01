9274d42935dad4d832005306c227849927537721
Edison Ditto <73842360+MedisonMan@users.noreply.github.com>
2021-12-01 00:30:40 -0600

    Update day1.py

--- 2021/day1.py
+++ 2021/day1.py
@@ -3,18 +3,16 @@ with open("Inputs/input1.txt") as f:
   data = [int(i) for i in f]
 
 # PART ONE
-print("PART ONE")
 total = 0
 for i in range(1, len(data)):
   if data[i-1] < data[i]:
     total += 1
-print(total)
+print("PART ONE:", total)
 
 # PART TWO
-print("PART TWO")
 total = 0
 window = data[:3]
 for i in range(1, len(data)):
   if sum(window) < sum( window := data[i:i+3]):
     total += 1
-print(total)
+print("PART TWO:", total)
