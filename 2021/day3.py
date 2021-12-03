40689032058870edededf9c4eb50b620ec59b153
Edison Ditto <73842360+MedisonMan@users.noreply.github.com>
2021-12-03 01:43:28 -0600

    Update day3.py

--- 2021/day3.py
+++ 2021/day3.py
@@ -6,25 +6,32 @@ def get_stuff(d, mostly, part):
   data = d.copy()
   binary = ""
   for i in range(12):
+    # Group the data into "every n-th item"
     bits = [*zip(*data)][i]
+    # Sort the bits by frequency
     common = [(i, bits.count(i)) for i in set(bits)]
     most = sorted(common, key=lambda x: x[1], reverse=mostly)
 
+    # If both numbers (0 and 1) are equally common...
     if most[0][1] == most[1][1]: most = "01"[mostly]
     else: most = most[0][0]
 
     if part == 2:
+      # Filter the new list by the character per position
       data = [j for j in data if j[i] == most]
+      # Stop searching
       if len(data) == 1:
         return int(data[0], 2)
 
+    # Add the most common character to the new binary number
     binary += most
+  # int(x, 2) turns binary into decimal (base 2)
   return int(binary, 2)
 
-gamma = get_stuff(data, True, 1)
-epsilon = get_stuff(data, False, 1)
+gamma = get_stuff(data, True, 1) # Using data from the file, sorted by most-common, part one
+epsilon = get_stuff(data, False, 1) # Using data from the file, sorted by least-common, part on1
 print("Part one:", gamma * epsilon)
 
-o2 = get_stuff(data, True, 2)
-co2 = get_stuff(data, False, 2)
+o2 = get_stuff(data, True, 2) # Data from file, most-common, part 2
+co2 = get_stuff(data, False, 2) # Data from file, least-common, part 2
 print("Part two:", o2 * co2)
