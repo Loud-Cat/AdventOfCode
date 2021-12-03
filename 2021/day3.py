33e5f80e31fe94fc7569c9837d46645ac77a6928
Edison Ditto <73842360+MedisonMan@users.noreply.github.com>
2021-12-03 01:23:08 -0600

    Update day3.py

--- 2021/day3.py
+++ 2021/day3.py
@@ -1,43 +1,30 @@
 with open("Inputs/input3.txt") as f:
   data = [i.strip() for i in f]
 
-# PART ONE
-def get_common(mostly):
+# PART TWO
+def get_stuff(d, mostly, part):
+  data = d.copy()
   binary = ""
   for i in range(12):
     bits = [*zip(*data)][i]
     common = [(i, bits.count(i)) for i in set(bits)]
     most = sorted(common, key=lambda x: x[1], reverse=mostly)
 
-    if most[0][1] == most[1][1]:
-      most = "01"[mostly]
-    else:
-      most = most[0][0]
+    if most[0][1] == most[1][1]: most = "01"[mostly]
+    else: most = most[0][0]
+
+    if part == 2:
+      data = [j for j in data if j[i] == most]
+      if len(data) == 1:
+        return int(data[0], 2)
 
     binary += most
   return int(binary, 2)
 
-gamma = get_common(True)
-epsilon = get_common(False)
+gamma = get_stuff(data, True, 1)
+epsilon = get_stuff(data, False, 1)
 print("Part one:", gamma * epsilon)
 
-# PART TWO
-def get_rating(d, mostly):
-  data = d.copy()
-  for i in range(12):
-    bits = [*zip(*data)][i]
-    common = [(i, bits.count(i)) for i in set(bits)]
-    most = sorted(common, key=lambda x: x[1], reverse=mostly)
-
-    if most[0][1] == most[1][1]:
-      most = "01"[mostly]
-    else:
-      most = most[0][0]
-
-    data = [j for j in data if j[i] == most]
-    if len(data) == 1:
-      return int(data[0], 2)
-
-o2 = get_rating(data, True)
-co2 = get_rating(data, False)
+o2 = get_stuff(data, True, 2)
+co2 = get_stuff(data, False, 2)
 print("Part two:", o2 * co2)
