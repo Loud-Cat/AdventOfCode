ea1a6391ba3c6ac5256c539fcdd896d0e13a621c
Edison Ditto <73842360+MedisonMan@users.noreply.github.com>
2021-12-03 00:48:39 -0600

    Update day3.py

--- 2021/day3.py
+++ 2021/day3.py
@@ -14,3 +14,24 @@ def get_common(mostly):
 gamma = get_common(True)
 epsilon = get_common(False)
 print("Part one:", gamma * epsilon)
+
+# PART TWO
+def get_rating(d, mostly):
+  data = d.copy()
+  for i in range(12):
+    bits = [*zip(*data)][i]
+    common = [(i, bits.count(i)) for i in set(bits)]
+    most = sorted(common, key=lambda x: x[1], reverse=mostly)
+
+    if most[0][1] == most[1][1]:
+      most = "01"[mostly]
+    else:
+      most = most[0][0]
+
+    data = [j for j in data if j[i] == most]
+    if len(data) == 1:
+      return int(data[0], 2)
+
+o2 = get_rating(data, True)
+co2 = get_rating(data, False)
+print("Part two:", o2 * co2)
