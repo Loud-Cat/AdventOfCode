67fe661f69a7caed50b533d9eb0fed62d48bcb27
Edison Ditto <73842360+MedisonMan@users.noreply.github.com>
2021-12-06 22:36:26 -0600

    Update day6.py

--- 2021/day6.py
+++ 2021/day6.py
@@ -2,21 +2,19 @@ with open("Inputs/input6.txt", "r") as f:
   data = [i.strip() for i in f]
   times = [int(i) for i in data[0].strip().split(",")]
 
-class Fish:
-  def __init__(self, t=9):
-    self.timer = t
+def get_population(initial_pop, days):
+  def day():
+    births = arr.pop(0)
+    arr[6] += births
+    arr.append(births)
 
-  def __repr__(self):
-    return f"{self.timer}"
-  
-  def wait(self):
-    self.timer -= 1
-    if self.timer < 0:
-      self.timer = 6
-      fish.append( Fish() )
+  arr = [0] * 9
+  for count in initial_pop:
+    arr[count] += 1
 
-fish = [Fish(t) for t in times]
-for i in range(80):
-  for f in fish:
-    f.wait()
-print("Part one:", len(fish))
+  for i in range(days):
+    day()
+  return sum(arr)
+
+print("Part one:", get_population(times, 80))
+print("Part two:", get_population(times, 256))
