9f39d4e1673869cf05ca50fedf0986230046f076
Edison Ditto <73842360+Loud-Cat@users.noreply.github.com>
2022-12-09 17:13:44 -0600

    Move Position to its own file

--- 2022/Day09/Day9.java
+++ 2022/Day09/Day9.java
@@ -76,29 +76,3 @@ public class Day9 {
         System.out.println( partOne() );
     }
 }
-
-class Position {
-    private int x, y;
-    
-    public Position(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-    
-    public int getX() { return x; }
-    public int getY() { return y; }
-    
-    @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof Position other)
-            return other.getX() == x && other.getY() == y;
-        
-        return false;
-    }
-    
-    @Override
-    public int hashCode() { return 0; }
-    
-    @Override
-    public String toString() { return String.format("(%d, %d)", x, y); }
-}
