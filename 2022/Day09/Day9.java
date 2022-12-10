fef018b7ae9717005d40ad8713eb70f879cf06e6
Edison Ditto <73842360+Loud-Cat@users.noreply.github.com>
2022-12-09 18:47:17 -0600

    Update Day9.java

--- 2022/Day09/Day9.java
+++ 2022/Day09/Day9.java
@@ -3,76 +3,60 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 
 import java.util.Set;
-import java.util.LinkedHashSet;
+import java.util.HashSet;
+
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
 
 /* Advent of Code
  * Day 9: Rope Bridge */
 
 public class Day9 {
-    public static boolean isTouching(int tx, int ty, int hx, int hy) {
-        /* Returns if the given tail is touching the given head */
-        
-        for (int x = tx - 1; x <= tx + 1; x++)
-            for (int y = ty - 1; y <= ty + 1; y++)
-                if (x == hx && y == hy)
-                    return true;
-        
-        return false;
-    }
-    
-    public static int partOne() throws IOException {
-        /* The first part of the puzzle
-         * This solution uses origin (0, 0)
-         * and "up" is an increase in y. */
-        
-        // starting position "s"
-        int headX = 0, headY = 0;
-        int tailX = 0, tailY = 0;
+    public static int[] bothParts(String file) throws IOException {
+        /* The second part of the puzzle */
         
         // a set to keep track of unique positions
-        Set<Position> visited = new LinkedHashSet<>();
-        visited.add( new Position(tailX, tailY) );
+        Set<Position> visitOne = new HashSet<>();
+        Set<Position> visitTwo = new HashSet<>();
         
-        BufferedReader reader = new BufferedReader( new FileReader("input.txt") );
+        // keep track of individual "tails"
+        List<Node> nodes = new ArrayList<>();
+        nodes.add( new Node(null) ); // head
+        
+        for (int i = 0; i < 9; i++)
+            nodes.add( new Node(nodes.get(i)) );
+        
+        Node tailOne = nodes.get(1); // follows H
+        Node tailTwo = nodes.get(9); // follows 8
+        
+        BufferedReader reader = new BufferedReader( new FileReader(file) );
         for (String line = reader.readLine(); line != null; line = reader.readLine()) {
             char direction = line.charAt(0);
             int amount = Integer.parseInt( line.substring(2) );
             
+            // calculate directions to move in
+            int dy = (direction == 'U') ? 1 : (direction == 'D') ? -1 : 0;
+            int dx = (direction == 'R') ? 1 : (direction == 'L') ? -1 : 0;
+            
+            // move
             for (int i = 0; i < amount; i++) {
-                if (direction == 'U') headY += 1;
-                else if (direction == 'D') headY -= 1;
+                tailTwo.move(dx, dy);
                 
-                else if (direction == 'L') headX -= 1;
-                else if (direction == 'R') headX += 1;
-                
-                // calculate difference in x and y
-                int dx = headX - tailX;
-                int dy = headY - tailY;
-                
-                /* "If the head is ever two steps directly
-                 * up, down, left, or right from the tail,
-                 * the tail must also move one step in that direction..." */
-                if (Math.abs(dx) == 2 && dy == 0)
-                    tailX += (tailX < headX) ? 1 : -1;
-                else if (Math.abs(dy) == 2 && dx == 0)
-                    tailY += (tailY < headY) ? 1 : -1;
-                
-                /* "Otherwise, if the head and tail aren't touching
-                 * and aren't in the same row or column,
-                 * the tail always moves one step diagonally" */
-                else if ( !isTouching(tailX, tailY, headX, headY) ) {
-                    if (dx != 0) tailX += (tailX < headX) ? 1 : -1;
-                    if (dy != 0) tailY += (tailY < headY) ? 1 : -1;
-                }
-                
-                visited.add( new Position(tailX, tailY) );
+                visitOne.add( new Position(tailOne.x, tailOne.y) );
+                visitTwo.add( new Position(tailTwo.x, tailTwo.y) );
             }
         }
         
-        return visited.size();
+        return new int[] {visitOne.size(), visitTwo.size()};
     }
     
     public static void main(String[] args) throws IOException {
-        System.out.println( partOne() );
+        long start = System.currentTimeMillis();
+        int[] solution = bothParts("input.txt");
+        long time = System.currentTimeMillis() - start;
+        
+        System.out.println( Arrays.toString(solution) );
+        System.out.printf("Completed in %d ms%n", time);
     }
 }
