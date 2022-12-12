1d7043ceebf476bea858749d498f06b9a56b4ea5
Edison Ditto <73842360+Loud-Cat@users.noreply.github.com>
2022-12-11 22:53:17 -0600

    Create Monkey.java

---
+++ 2022/Day11/Monkey.java
@@ -0,0 +1,60 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.function.IntUnaryOperator;
+
+public class Monkey {
+    private static final List<Monkey> monkeys = new ArrayList<>();
+    
+    List<Item> items;
+    IntUnaryOperator operation;
+    int test;
+    int yes, no;
+    long total;
+    
+    public Monkey(List<Item> items, IntUnaryOperator op, int test, int yes, int no) {
+        this.items = items;
+        
+        this.operation = op;
+        
+        this.test = test;
+        this.yes = yes;
+        this.no = no;
+        
+        this.total = 0;
+    }
+    
+    public static void init(List<Monkey> monkeyz) {
+        monkeys.clear();
+        monkeys.addAll(monkeyz);
+    }
+    
+    public void add(Item item) { items.add(item); }
+    
+    public void inspectAll() {
+        for (Item item : items) {
+            item.worry = operation.applyAsInt(item.worry) / 3;
+            int index = (item.worry % test == 0) ? yes : no;
+            monkeys.get(index).add(item);
+            
+            total++;
+        }
+        
+        items.clear();
+    }
+    
+    public void inspectAll(int modulo) {
+        for (Item item : items) {
+            item.worry = operation.applyAsInt(item.worry) % modulo;
+            int index = (item.worry % test == 0) ? yes : no;
+            monkeys.get(index).add(item);
+            
+            total++;
+        }
+        
+        items.clear();
+    }
+    
+    public String toString() {
+        return String.format("{items=%s, test=%d, yes=%d, no=%d, total=%d}", items, test, yes, no, total);
+    }
+}
